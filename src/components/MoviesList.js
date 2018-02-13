7cae3ca0fdef07096aaf2b9ebcb992b49f97343c
edwin rivera <waehake@gmail.com>
2018-02-13 00:14:42 +0000

    automatically backed up by learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default (props) => {
   const movies = props.movies.map(movie => <li key={movie.id}>{movie.title}</li>);
