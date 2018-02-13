3ec976b339d09c56ac982e7655b7cd75320e0f3e
edwin rivera <waehake@gmail.com>
2018-02-13 00:17:00 +0000

    automatically backed up by learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -11,18 +11,7 @@ const MoviesList = ({ movies }) => {
     <div>
       {renderMovies}
     </div>
-  )
-}
-
-export default (props) => {
-  const movies = props.movies.map(movie => <li key={movie.id}>{movie.title}</li>);
-  return (
-    <div>
-      <div className='col-md-4'>
-        <ul>
-          {movies}
-        </ul>
-      </div>
-    </div>
   );
 };
+
+export default MoviesList;
