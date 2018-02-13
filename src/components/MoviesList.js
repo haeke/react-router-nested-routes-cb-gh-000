f814762145312a627467072c017ffb50d52e3931
edwin rivera <waehake@gmail.com>
2018-02-13 00:16:05 +0000

    automatically backed up by learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MoviesList = ({movies}) => {
+const MoviesList = ({ movies }) => {
   const renderMovies = movies.map(movie =>
     <Link key={movie.id} to={`/movies/${movie.id}`}>
     {movie.title}</Link>
