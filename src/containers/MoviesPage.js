1e3022f4edfe7a75d5beeeab05919f628d637bfc
edwin rivera <waehake@gmail.com>
2018-02-13 00:43:43 +0000

    automatically backed up by learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -12,6 +12,7 @@ const MoviesPage = ({ match, movies }) =>
   <div>
     <MovieList movies={movies} />
     <Route exact path={`${match.url}/:movieId`} component={MovieShow} />
+    <Route exact path={match.url} render={() => ( <h3>Please select a Movie from the list.</h3>)} />
   </div>
 
 const mapStateToProps = (state) => {
